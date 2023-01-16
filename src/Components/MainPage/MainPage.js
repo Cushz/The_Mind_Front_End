73b7fd20a517474b65da9fd48aa4dc686b12ac82
HorrorOTB <nicatabdullazade135@gmail.com>
2023-01-16 16:24:13 +0400

    Added Rules button

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -8,6 +8,7 @@ const MainPage = () => {
     <>
     <div className='mainpage-container'>
       <Button  width="11.688rem" height='4.25rem' color='#16163D' fontSize='2.063rem' fontFamily='Montserrat' borderRadius='0.75rem' right='36.5rem' top='8.438rem' background='rgba(255, 255, 255, 0.87)' boxShadow='6px 4px 15px rgba(11, 11, 52, 0.25);'>Play</Button>
+      <Button  width="11.688rem" height='4.25rem' color='#16163D' fontSize='2.063rem' fontFamily='Montserrat' borderRadius='0.75rem' right='20.5rem' top='4.25rem' background='rgba(255, 255, 255, 0.87)' boxShadow='6px 4px 15px rgba(11, 11, 52, 0.25);'>Rules</Button>
     </div>
     
     
